fabafb0935c055b1e187dc8f1b09af0022bb788a
xandervdh <xander-v.d.h@hotmail.com>
2020-11-05 10:14:12 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+require_once 'vendor/autoload.php';
+
+
+require_once 'buttons.html';
